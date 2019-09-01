69daeb73eba38dc6dfc308c1ea5a57b108187dae
Eng-ALFathi <Eng.ALFathi.2020@gmail.com>
2019-09-01 19:47:40 +0200

    Two Modules userlog & usertype

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require_once "path.php";
 
 require_once MODEL."/userModel.php";
 require_once MODEL."/courseCategoryModel.php";
+require_once MODEL."/userlogModel.php";
+require_once MODEL."/usertypeModel.php";
 
 /* UserModel */
 $user = new userModel();
@@ -21,20 +23,72 @@ $courseCat=new courseCategoryModel();
 $courseCatData = ['id','category_name'];
 $courseCatWhere = array('id'=>3);
 echo '<pre>';
-print_r($courseCat->select($courseCatData,$courseCatWhere));
+//print_r($courseCat->select($courseCatData,$courseCatWhere));
 echo '</pre>';
 //Insert..
 $courseCatInsert = ['category_name'=>'Ajax'];
-print_r($courseCat->insert($courseCatInsert));
+//print_r($courseCat->insert($courseCatInsert));
 //Update..
 $courseCatUpdate = ['category_name'=>'Ruby'];
 $courseCatWhereUpdate = array('id'=>3);
 echo '<pre>';
-print_r($courseCat->update($courseCatUpdate,$courseCatWhereUpdate));
+//print_r($courseCat->update($courseCatUpdate,$courseCatWhereUpdate));
 echo '</pre>';
 //Delete..
 $courseCatWhereDelete = array('id'=>2);
 echo '<pre>';
-print_r($courseCat->delete($courseCatWhereDelete));
+//print_r($courseCat->delete($courseCatWhereDelete));
 echo '</pre>';
 /* CourseCategoryModel */
+
+/* UserLogModel */
+$userLog= new userlogModel();
+//Select..
+$userLogData = ['user_id','user_type_id','user_name','user_password'];
+$userLogWhere = array('user_name ='=>'ahmed','and user_password ='=> '789');
+echo '<pre>';
+//print_r($userLog->select($userLogData,$userLogWhere));
+echo '</pre>';
+//Insert..
+$userLogInsert = ['user_id'=>'4','user_type_id'=>'1','user_name'=>'ahmed','user_password'=>'789'];
+//print_r($userLog->insert($userLogInsert));
+//Update..
+$userLogUpdate = ['user_password'=> 'kkk'];
+$userLogWhereUpdate = array('user_id'=>4);
+echo '<pre>';
+//print_r($userLog->update($userLogUpdate,$userLogWhereUpdate));
+echo '</pre>';
+//Delete..
+$userLogWhereDelete = array('id'=>8);
+echo '<pre>';
+//print_r($userLog->delete($userLogWhereDelete));
+echo '</pre>';
+/* UserLogModel */
+
+/* UserTypeModel */
+$usertype= new usertypeModel();
+//Select Without where..
+echo '<pre>';
+//print_r($usertype->Query());
+echo '</pre>';
+//Select..
+$usertypeData = ['id','type_title'];
+$usertypeWhere = array('type_title ='=>'admin');
+echo '<pre>';
+//print_r($usertype->select($usertypeData,$usertypeWhere));
+echo '</pre>';
+//Insert..
+$usertypeInsert = ['type_title'=>'employee'];
+//print_r($usertype->insert($usertypeInsert));
+//Update..
+$usertypeUpdate = ['type_title'=> 'Employee'];
+$usertypeWhereUpdate = array('id'=>5);
+echo '<pre>';
+//print_r($usertype->update($usertypeUpdate,$usertypeWhereUpdate));
+echo '</pre>';
+//Delete..
+$usertypeWhereDelete = array('id'=>5);
+echo '<pre>';
+//print_r($usertype->delete($usertypeWhereDelete));
+echo '</pre>';
+/* UserTypeModel */
--- model/userlogModel.php
+++ model/userlogModel.php
@@ -1,8 +1,8 @@
 <?php
-
 require_once "core.php";
 require_once "config.php";
-class courseCategoryModel implements core
+
+class userlogModel implements core
 {
     private $db;
     public function __construct()
@@ -10,19 +10,21 @@ class courseCategoryModel implements core
         global $dbMyConfig;
         $this->db = new PdoWrapper($dbMyConfig);
     }
+
     public function Query(){}
     public function select($data,$where){
-       $res = $this->db->select('course_category',$data,$where)->results();
+        $res = $this->db->select('user_login',$data,$where)->results();
 
         if(count($res)>0){
             PDOHelper::PA($res);
+            return"Login succeeded";
         }else{
-            return "Error";
+            return "Check Your Username or Password";
         }
 
     }
     public function insert($data){
-        $res = $this->db->insert('course_category',$data)->getLastInsertId();
+        $res=$this->db->insert('user_login',$data)->getLastInsertId();
         if($res>0){
             return "New Row ID Is: ".$res;
         }else{
@@ -30,7 +32,7 @@ class courseCategoryModel implements core
         }
     }
     public function update($data,$where){
-        $res = $this->db->update('course_category', $data, $where)->affectedRows();
+        $res = $this->db->update('user_login', $data, $where)->affectedRows();
         if($res>0){
             return "Number Of Rows Updated Is: ".$res;
         }else{
@@ -38,8 +40,7 @@ class courseCategoryModel implements core
         }
     }
     public function delete($id){
-
-        $res = $this->db->delete('course_category', $id)->affectedRows();
+        $res = $this->db->delete('user_login', $id)->affectedRows();
         if($res>0){
             return "Number Of Rows Deleted Is: ".$res;
         }else{
